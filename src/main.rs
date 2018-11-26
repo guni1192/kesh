62de952c49ff593894bd87fcbe5ca53ee6beed70
guni973 <ad2314ce71926@gmail.com>
2018-11-27 03:39:42 +1100

    add execv wrapper

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,30 @@ fn split_cmd(command: String, cmds: &mut Vec<CString>) {
     }
 }
 
+fn execv_wrapper(line: String) {
+    let mut cmds = Vec::<CString>::new();
+    split_cmd(line.clone(), &mut cmds);
+
+    println!("{:?}", cmds.clone());
+
+    match fork() {
+        Ok(ForkResult::Parent { child, .. }) => {
+            match waitpid(child, None).expect("waitpid faild") {
+                WaitStatus::Exited(_, _) => {}
+                WaitStatus::Signaled(_, _, _) => {}
+                _ => eprintln!("Unexpected exit."),
+            }
+        }
+        Ok(ForkResult::Child) => {
+            match execv(&cmds[0], &cmds) {
+                Ok(_) => {}
+                Err(_) => println!("Command Not Found: {:?}", cmds.clone()),
+            };
+        }
+        Err(err) => eprintln!("Fork faild: {}", err),
+    }
+}
+
 fn print_prompt() {
     print!(">> ");
 }
@@ -30,27 +54,7 @@ fn main() {
         print_prompt();
         io::stdout().flush().unwrap();
 
-        let s = input_cmd();
-        let mut cmds = Vec::<CString>::new();
-        split_cmd(s.clone(), &mut cmds);
-
-        println!("{:?}", cmds.clone());
-
-        match fork() {
-            Ok(ForkResult::Parent { child, .. }) => {
-                match waitpid(child, None).expect("waitpid faild") {
-                    WaitStatus::Exited(_, _) => {}
-                    WaitStatus::Signaled(_, _, _) => {}
-                    _ => eprintln!("Unexpected exit."),
-                }
-            }
-            Ok(ForkResult::Child) => {
-                match execv(&cmds[0], &cmds) {
-                    Ok(_) => {}
-                    Err(_) => println!("Command Not Found: {}", s.clone()),
-                };
-            }
-            Err(err) => eprintln!("Fork faild: {}", err),
-        }
+        let line = input_cmd();
+        execv_wrapper(line);
     }
 }
