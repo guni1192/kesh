6c52b18ac9e4a610d9d74fc32ac2911aea19bc9b
guni973 <ad2314ce71926@gmail.com>
2018-11-27 02:45:56 +1100

    make Interactive

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,33 @@
 use std::env;
 use std::ffi::CString;
+use std::io::{self, Write};
 
 use nix::unistd::execv;
 
-pub fn input_cmd() -> String {
+fn input_cmd() -> String {
     let mut s = String::new();
     std::io::stdin().read_line(&mut s).expect("hoge");
     s.trim().parse().expect("foo")
 }
 
+fn print_prompt() {
+    print!(">> ");
+}
+
 fn main() {
     env::set_var("RUST_BACKTRACE", "1");
 
-    let s = input_cmd();
-    let cmd = CString::new(s.clone()).expect("Could not parse string to char string");
+    loop {
+        print_prompt();
+        io::stdout().flush().unwrap();
 
-    execv(&cmd, &[cmd.clone()]).expect(&format!("Command Not Found: {}", s.clone()));
+        let s = input_cmd();
+        let cmd = CString::new(s.clone()).expect("Could not parse string to char string");
+
+        // execv(&cmd, &[cmd.clone()]).expect(&format!("Command Not Found: {}", s.clone()));
+        match execv(&cmd, &[cmd.clone()]) {
+            Ok(_) => {}
+            Err(_) => println!("Command Not Found: {}", s.clone()),
+        };
+    }
 }
