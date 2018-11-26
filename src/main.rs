804d38602a545fad71d2d4c58eae6a4440ccb526
guni973 <ad2314ce71926@gmail.com>
2018-11-27 02:37:58 +1100

    initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::env;
+use std::ffi::CString;
+
+use nix::unistd::execv;
+
+pub fn input_cmd() -> String {
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).expect("hoge");
+    s.trim().parse().expect("foo")
+}
+
 fn main() {
-    println!("Hello, world!");
+    env::set_var("RUST_BACKTRACE", "1");
+
+    let s = input_cmd();
+    let cmd = CString::new(s.clone()).expect("Could not parse string to char string");
+
+    execv(&cmd, &[cmd.clone()]).expect(&format!("Command Not Found: {}", s.clone()));
 }
