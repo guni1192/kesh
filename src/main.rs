01735d7a44b6fe87044120c8714d168b8e173c37
guni973 <ad2314ce71926@gmail.com>
2018-11-27 03:32:45 +1100

    fork(2) previous execv(2)

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,21 @@ use std::env;
 use std::ffi::CString;
 use std::io::{self, Write};
 
-use nix::unistd::execv;
+use nix::sys::wait::{waitpid, WaitStatus};
+use nix::unistd::{execv, fork, ForkResult};
 
 fn input_cmd() -> String {
     let mut s = String::new();
     std::io::stdin().read_line(&mut s).expect("hoge");
     s.trim().parse().expect("foo")
+}
+
+fn split_cmd(command: String, cmds: &mut Vec<CString>) {
+    let iter = command.split_whitespace();
+    for x in iter {
+        let c = CString::new(x).expect("Could not parse string to char string");
+        cmds.push(c);
+    }
 }
 
 fn print_prompt() {
@@ -22,12 +31,26 @@ fn main() {
         io::stdout().flush().unwrap();
 
         let s = input_cmd();
-        let cmd = CString::new(s.clone()).expect("Could not parse string to char string");
+        let mut cmds = Vec::<CString>::new();
+        split_cmd(s.clone(), &mut cmds);
 
-        // execv(&cmd, &[cmd.clone()]).expect(&format!("Command Not Found: {}", s.clone()));
-        match execv(&cmd, &[cmd.clone()]) {
-            Ok(_) => {}
-            Err(_) => println!("Command Not Found: {}", s.clone()),
-        };
+        println!("{:?}", cmds.clone());
+
+        match fork() {
+            Ok(ForkResult::Parent { child, .. }) => {
+                match waitpid(child, None).expect("waitpid faild") {
+                    WaitStatus::Exited(_, _) => {}
+                    WaitStatus::Signaled(_, _, _) => {}
+                    _ => eprintln!("Unexpected exit."),
+                }
+            }
+            Ok(ForkResult::Child) => {
+                match execv(&cmds[0], &cmds) {
+                    Ok(_) => {}
+                    Err(_) => println!("Command Not Found: {}", s.clone()),
+                };
+            }
+            Err(err) => eprintln!("Fork faild: {}", err),
+        }
     }
 }
