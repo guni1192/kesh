0a154a482ec927bb97553c002c00abf14d53b1e5
guni1192 <ad2314ce71926@gmail.com>
2018-11-28 05:19:00 +0900

    雑にパス解決

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 use std::ffi::CString;
 use std::io::{self, Write};
+use std::path::Path;
 
 use nix::sys::wait::{waitpid, WaitStatus};
 use nix::unistd::{execve, fork, ForkResult};
@@ -13,7 +14,11 @@ fn input_cmd() -> String {
 }
 
 fn split_cmd(command: String, cmds: &mut Vec<CString>) {
-    let iter = command.split_whitespace();
+    let mut iter = command.split_whitespace();
+    let cmd = realpath_from_string(iter.next().unwrap().to_string()).unwrap();
+    let cmd = CString::new(cmd).expect("Could not parse string to char string");
+    cmds.push(cmd);
+
     for x in iter {
         let c = CString::new(x).expect("Could not parse string to char string");
         cmds.push(c);
@@ -52,6 +57,27 @@ fn print_prompt() {
     print!(">> ");
 }
 
+fn realpath_from_string(cmd: String) -> Result<String, String>{
+    let key = "PATH";
+    match env::var_os(key) {
+        Some(paths) => {
+            for path in env::split_paths(&paths) {
+                let cmd_full_path = format!("{}/{}", path.to_str().unwrap(), cmd);
+
+                if Path::new(&cmd_full_path).exists() {
+                    return Ok(cmd_full_path)
+                }
+
+            }
+            Err(format!("{} not found.", cmd))
+        }
+        None => Err(format!("{} not found.", cmd))
+    }
+
+}
+
+
+
 fn main() {
     env::set_var("RUST_BACKTRACE", "1");
 
@@ -65,4 +91,9 @@ fn main() {
         let environ = CString::new(environ).expect("Failed to convert string to char string: ");
         execv_wrapper(line, environ);
     }
+}
+
+#[test]
+fn test_realpath_from_path() {
+    assert_eq!(realpath_from_string("bash".to_string()).unwrap(), "/bin/bash".to_string());
 }
