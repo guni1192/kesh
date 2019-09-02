0f3f71e3757dc0c0ab5b997b6412220201416c18
guni1192 <ad2314ce71296@gmail.com>
2019-09-02 16:23:17 +0900

    Use borrow instead of clone

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,8 @@ use nix::unistd::{execve, fork, ForkResult};
 mod buildin;
 mod prompt;
 
+static DEFAULT_PATH_ENV: &'static str = "PATH";
+
 fn input_cmd() -> String {
     let mut s = String::new();
     std::io::stdin()
@@ -17,13 +19,13 @@ fn input_cmd() -> String {
     s.trim().parse().expect("Cannot parse trim str")
 }
 
-fn split_cmd(command: String) -> Vec<CString> {
+fn split_cmd(command: &str) -> Vec<CString> {
     let mut args = Vec::<CString>::new();
     let args_tmp: Vec<&str> = command.split_whitespace().collect();
 
     for i in 0..args_tmp.len() {
         let cmd = if i == 0 {
-            CString::new(realpath_from_string(args_tmp[i].to_string()))
+            CString::new(realpath_from_string(&args_tmp[i]))
         } else {
             CString::new(args_tmp[i])
         };
@@ -33,8 +35,8 @@ fn split_cmd(command: String) -> Vec<CString> {
     args
 }
 
-fn execv_wrapper(line: String, path: CString) {
-    let args = split_cmd(line.clone());
+fn execv_wrapper(line: &str, path: CString) {
+    let args = split_cmd(line);
 
     if args.len() == 0 {
         return;
@@ -48,23 +50,22 @@ fn execv_wrapper(line: String, path: CString) {
         Ok(ForkResult::Parent { child, .. }) => {
             match waitpid(child, None).expect("waitpid faild") {
                 WaitStatus::Exited(_, _) => {}
-                WaitStatus::Signaled(_, _, _) => {}
+                WaitStatus::Signaled(_, _, _) => {
+                    // TODO: SIGKILL, SIGTERM
+                }
                 _ => eprintln!("Unexpected exit."),
             }
         }
         Ok(ForkResult::Child) => {
-            match execve(&args[0], &args, &[path]) {
-                Ok(_) => {}
-                Err(_) => eprintln!("{:?} not found.", args[0].clone()),
-            };
+            execve(&args[0], &args, &[path])
+                .unwrap_or_else(|e| panic!("Error while execve: {}", e));
         }
         Err(err) => eprintln!("Fork faild: {}", err),
     }
 }
 
-fn realpath_from_string(cmd: String) -> String {
-    let key = "PATH";
-    match env::var_os(key) {
+fn realpath_from_string(cmd: &str) -> String {
+    match env::var_os(DEFAULT_PATH_ENV) {
         Some(paths) => {
             for path in env::split_paths(&paths) {
                 let cmd_full_path = format!("{}/{}", path.to_str().unwrap(), cmd);
@@ -73,9 +74,9 @@ fn realpath_from_string(cmd: String) -> String {
                     return cmd_full_path;
                 }
             }
-            cmd
+            cmd.to_string()
         }
-        None => cmd,
+        None => cmd.to_string(),
     }
 }
 
@@ -90,14 +91,11 @@ fn main() {
         let path = env::var("PATH").expect("Environment value not found:");
         let environ = format!("PATH={}", path);
         let environ = CString::new(environ).expect("Failed to convert string to char string: ");
-        execv_wrapper(line, environ);
+        execv_wrapper(&line, environ);
     }
 }
 
 #[test]
 fn test_realpath_from_path() {
-    assert_eq!(
-        realpath_from_string("bash".to_string()),
-        "/bin/bash".to_string()
-    );
+    assert_eq!(realpath_from_string("bash"), "/bin/bash".to_string());
 }
